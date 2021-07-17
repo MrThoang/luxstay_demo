24a563572718cc063fb2d063217f493f1848089c
MrThoang <hoangthoang0309@gmail.com>
2021-07-17 23:52:39 +0700

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,10 @@
-var search =  document.getElementsByClassName('search-content')
-let date =  document.getElementsByClassName('header__search-date')
-let guests =  document.getElementsByClassName('header__search-guests')
-
-    search[0].addEventListener('click', function e(prop){
-        prop.stopPropagation()
-        this.classList.toggle('toggle-css');
-    })
+/* Modal-date */
+const Date = document.querySelector('.js-date-btn')
+const containerDate = document.querySelector('.datepicker-wrapper')
 
 
+function showDate() {
+    containerDate.classList.add('open')
+}
 
+Date.addEventListener('click', showDate)
